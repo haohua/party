ead39f19fe4665516d1be7ce6d1ab1965b029908
atang1 <atang1@morningstar.com>
2013-11-21 13:21:13 -0600

    add example output to the install packages

--- install_Packages.R
+++ install_Packages.R
@@ -33,14 +33,45 @@ rfout <- mobForestAnalysis(as.formula(medv ~ lstat),
                                                
                                                objfun = correl.obj,
                                                # here the correl.obj is embeded in the modeltools, but you may supply your own objfun
-                                               
+                                               # also the correl.obj is calcualting the negative correlation ( for minimizing)
                                                minsplit = 25),
                            data = BostonHousing, processors = 3, model = linearModel)
 
 pred = getPredictedValues(object=rfout, newdata=T, 
                           newTestData=BostonHousing[1, ])
-
 #
 # in the pred result, you may check statistics for each tree, each single node, and some average stat accross the trees
 # documentation is not supplied, please let me know if there is any question. 
 
+# # eg.
+# > pred
+# response
+# [,1]     [,2]     [,3]
+# [1,] 24.95467 29.74449 29.03649
+# 
+# $node
+# [,1] [,2] [,3]
+# [1,]   12    7    1
+# 
+# $nodeAdjR2
+# [,1]      [,2]     [,3]
+# [1,] 0.3608695 0.5866803 0.519687
+# 
+# $nodeObjFunValue
+# [,1]       [,2]       [,3]
+# [1,] -0.6809822 -0.7729968 -0.8159765
+# 
+# $predMat
+# PredMean PredStdev  Residual AvgNodeAdjR2 AvgNodeObjFunValue
+# [1,] 27.91188  2.585372 -3.911881    0.4890789         -0.7566518
+# 
+# $predConditions
+# $predConditions[[1]]
+# tax_LessThan tax_MoreThan nodeID objFunValue     AdjR2 crim_LessThan crim_MoreThan
+# 1          305          277     12  -0.6809822 0.3608695            NA            NA
+# 2           NA          242      7  -0.7729968 0.5866803            NA            NA
+# 3           NA           NA      1  -0.8159765 0.5196870            NA            NA
+# tree_id rad_LessThan rad_MoreThan
+# 1       1           NA           NA
+# 2       2            2           NA
+# 3       3           NA           NA
